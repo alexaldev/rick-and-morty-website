5aac5da9d1faef27344d64f55aa1e195cb417a76
nikniknik <nikniknik@csd.auth.gr>
2018-01-16 00:39:44 +0200

    Quiz in progress

--- Quiz.php
+++ Quiz.php
@@ -3,8 +3,42 @@
 $page_title = "Quiz";
 $nav_curr = "Quiz";
 
+// number of questions to be appeared ni site
+$n_questions = 2;
+
+// number of total questions in db
+
+// Return N unique random numbers in range min-max
+function getNrandomNumbers($N, $min, $max) {
+
+	// Optimized solution for picking few numbers from a big pool
+	if (($max-$min) - $N > 200) {
+		$random_arr = [];
+		while (count($random_arr) < $N) {
+			$random = mt_rand($min, $max);
+			$random_arr[$random] = $random;
+		}
+		return array_values($random_arr);
+	}
+	// Solution for smaller scale
+	else {
+		$random_arr = range($min, $max);
+		shuffle($random_arr);
+		return array_slice($random_arr, 0, $N);
+	}
+}
+
+function showQuiz() {
+
+	session_start();
+	echo '<p>We have a session</p>';
+
+
+}
+
 function getPageContent() {
 	echo '<p>Quiz page! (Under construction)</p>';
+	showQuiz();
 }
 
 include('resources/template.php');
--- resources/comments_schema.sql
+++ resources/comments_schema.sql
@@ -15,3 +15,26 @@ UPDATE comments SET nickname = "birdperson17" WHERE commentID = 1;
 INSERT INTO comments (comment_text)
 VALUES ('This should be the official Rick and Morty site... For real!11!!');
 UPDATE comments SET nickname = "1213" WHERE commentID = 2;
+
+CREATE TABLE quiz (
+	quizID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	question TEXT,
+	possible_answers TEXT,
+	right_answer_index INT
+);
+
+INSERT INTO quiz (question, possible_answers, right_answer_index)
+VALUES ('Question1', 'answer1;answer2;answer3;answer4', 1);
+
+INSERT INTO quiz (question, possible_answers, right_answer_index)
+VALUES ('Question2', 'answer1;answer2;answer3', 1);
+
+INSERT INTO quiz (question, possible_answers, right_answer_index)
+VALUES ('Question3', 'answer1;answer2;answer3', 1);
+
+INSERT INTO quiz (question, possible_answers, right_answer_index)
+VALUES ('Question4', 'answer1;answer2;answer3;answer4;answer5', 1);
+
+
+GRANT select, insert ON rick_and_morty_website.comments TO 'nikiforos'@'localhost';
+GRANT select ON rick_and_morty_website.quiz TO 'nikiforos'@'localhost';
