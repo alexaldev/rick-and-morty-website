b66ab614b19893b9178349e42f1590becb4f5219
nikniknik <nikniknik@csd.auth.gr>
2018-01-24 01:08:34 +0200

    Quiz progress

--- Quiz.php
+++ Quiz.php
@@ -24,7 +24,8 @@ function getNrandomNumbers($N, $min, $max) { //EDW XREIAZETAI MALLON -1 AMA BUGG
 	else {
 		$random_arr = range($min, $max);
 		shuffle($random_arr);
-		return array_slice($random_arr, 0, $N);
+		$random_arr = array_slice($random_arr, 0, $N);
+		return $random_arr;
 	}
 }
 
@@ -32,14 +33,20 @@ function showQuiz() {
 
 	require("resources/connection.php");
 
-	$count_ans = mysqli_query($mysql_link, "SELECT COUNT(questionID) AS num_rows FROM quiz");
-	$total_num_questions = mysqli_fetch_object($count_ans)->num_rows;
+	$count_ans = mysqli_query($link, "SELECT COUNT(questionID) AS num_rows FROM quiz");
+	$total_num_questions = mysqli_fetch_row($count_ans)[0];
+	
+	$random_arr = getNrandomNumbers($n_questions, 1, $total_num_questions);
 
-	$random_arr = getNrandomNumbers($n_questions, 0, $total_num_questions);
+	echo "<p>Random numbers: </p>";
+	foreach ($random_arr as $num) {
+		echo "$num; ";
+	}
 
 	session_start();
+	$_SESSION['test'] = $random_arr[0];
 	echo '<p>We have a session</p>';
-
+	echo '<p>Here is your random number -> ' . $_SESSION['test'] . '.</p>';
 
 }
 
