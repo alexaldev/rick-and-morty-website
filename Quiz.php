21170105a5c979f0e55fec9f9f5f1762adf1fa74
nikniknik <nikniknik@csd.auth.gr>
2018-01-24 18:37:51 +0200

    Quiz added

--- Quiz.php
+++ Quiz.php
@@ -38,27 +38,80 @@ function initializeQuiz($n_questions) {
 	
 	$random_arr = getNrandomNumbers($n_questions, 1, $total_num_questions);
 
-	session_start();
-	$_SESSION['test'] = $random_arr[0];
+	unset($_SESSION['quiz_terminated']);
+	$_SESSION['rand_nums'] = $random_arr;
+	$_SESSION['right_answers'] = array();
 
+	echo "<html><body>";
+	generate_question();
+	echo "</body></html>";
+/*
 	echo "<html><body><div class=quiz_question>";
+
 	echo "<p>Random numbers: </p>";
 	foreach ($random_arr as $num) {
 		echo "$num; ";
 	}
-	echo '<p>We have a session ' . session_id() . '</p>';
-	echo '<p>I give you this random number -> ' . $_SESSION['test'] . '.</p>';
+
 	echo "</div></body></html>";
+*/
+}
+
+function generate_question() {
+
+	require("resources/connection.php");
+
+//	session_start();
+
+	if (count($_SESSION['rand_nums']) == 0) {
+		// Quiz finished
+
+		$_SESSION['quiz_terminated'] = 1;
+
+		$score = array_sum($_SESSION['right_answers']) / count($_SESSION['right_answers']);
+		$score *= 100;
+		
+
+		echo "<div class=\"current quiz_results\">
+				<p>You terminated it!</p>
+			  	<p>Your score is $score%!</p>
+			  </div>";
+
+		return;
+	}
+		
+
+	$rand_num =  array_pop($_SESSION['rand_nums']);
+	$question_ans = mysqli_query($link, "SELECT question, possible_answers, right_answer_index FROM quiz WHERE questionID = $rand_num");
+	$row = mysqli_fetch_assoc($question_ans);
+
+	$question = $row['question'];
+	$answers = explode(';', $row['possible_answers']);
+	$right_answer_index = $row['right_answer_index']-1;
+
+	$_SESSION['right_answer_index'] = $right_answer_index;
+?>
+	<div class="current quiz_question">
+		<?php echo "<p> $question </p>";
+			echo "<div class=\"answers\">";
+			foreach($answers as $i=>$ans)
+				echo "<div class=\"answer_choice\"><input type=\"radio\" name=\"answer\" value=\"$i\"><p>$ans</p></div>";
+			echo "</div>";
+		?>
+	</div>
+<?php
 }
 
 function check_and_reply_question() {
 
-	session_start();
-	echo "<html><body><div class=quiz_question>";
-	echo '<p>Here is your random number -> ' . $_SESSION['test'] . '.</p>';
-	echo "</div></body></html>";
-}
+	array_push($_SESSION['right_answers'], $_POST['answer'] == $_SESSION['right_answer_index']);
 
+
+	echo "<html><body>";
+	echo "<span id=\"right_answer_index\">" . $_SESSION['right_answer_index'] . "</span>";
+	generate_question();
+	echo "</body></html>";
+}
 
 function getHeadContent() {
 	echo '<script type="text/javascript" src="js/quiz.js"></script>';
@@ -77,11 +130,13 @@ function getPageContent() {
 
 }
 
-if (!isset($_POST['startQuiz']) && !isset($_COOKIE['PHPSESSID']))
-	include('resources/template.php');
-else if (isset($_POST['startQuiz']) && $_POST['startQuiz'] == 1)
+session_start();
+
+if (isset($_POST['startQuiz']) && $_POST['startQuiz'] == 1)
 	initializeQuiz($n_questions);
-else
+else if (isset($_COOKIE['PHPSESSID']) && isset($_POST['answer']) && !isset($_SESSION['quiz_terminated']))
 	check_and_reply_question();
+else
+	include('resources/template.php');
 
 ?>
--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,57 @@
 
 
+function checkAnswer(event) {
+
+	var currAnsName = event.target.name;
+	var currAnsValue = event.target.value;
+	var currAnsDiv = event.target.parentNode;
+	var allAnswers = currAnsDiv.parentNode;
+	var currQuizQuestion = allAnswers.parentNode;
+	var quizContainer = currQuizQuestion.parentNode;
+	
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 )
+			if (request.status == 200) {
+				var parser = new DOMParser();
+				var doc = parser.parseFromString(request.responseText, "text/html");
+
+				var right_ans = doc.getElementById('right_answer_index').textContent;
+				
+				var nextQuestion = doc.getElementsByClassName('current')[0];
+
+				if (currAnsValue == right_ans)
+					currAnsDiv.style.backgroundColor = "green";
+				else
+					currAnsDiv.style.backgroundColor = "red";
+
+				currQuizQuestion.classList.remove('current');
+				quizContainer.appendChild(nextQuestion);
+
+				if (nextQuestion.classList.contains('quiz_question')) 
+					addRadioListeners();
+			}
+	}
+	request.open('POST', 'Quiz.php', true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send(currAnsName + "=" + currAnsValue);
+
+	var currButtonDivs = allAnswers.getElementsByClassName('answer_choice');
+	for (var i=0; i<currButtonDivs.length; i++)
+		currButtonDivs[i].getElementsByTagName('input')[0].disabled = true;
+}
+
+function addRadioListeners() {
+	var currButtonDivs = document
+		.querySelector('.current.quiz_question .answers')
+		.getElementsByClassName('answer_choice');
+
+	for (var i=0; i<currButtonDivs.length; i++)
+		currButtonDivs[i]
+			.getElementsByTagName('input')[0]
+			.addEventListener('click', checkAnswer);
+}
+
 function requestQuizStart(event) {
 
 	var request = new XMLHttpRequest();
@@ -12,20 +64,16 @@ function requestQuizStart(event) {
 
 				var quizContainer = document.getElementsByClassName('quiz_container')[0];
 				quizContainer.replaceChild(firstQuestion, event.target);
+				addRadioListeners();
 			}
 	}
 	request.open('POST', 'Quiz.php', true);
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	request.send("startQuiz=1");
-
-	
-
 }
 
-function addListeners() {
-	var quizButton = document.getElementById('quiz_start_button');
-	
-	quizButton.addEventListener('click', requestQuizStart);
-}
-
-window.onload = addListeners;
+window.onload = function() {
+	document
+	.getElementById('quiz_start_button')
+	.addEventListener('click', requestQuizStart);
+};
